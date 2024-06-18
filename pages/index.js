1070e23cb1e05805aff4af56ded69d2286cbe7ce
Diego Sadock <95287173+diegosadock@users.noreply.github.com>
2024-06-18 20:38:17 +0000

    adiciona o gif da mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
-    return ```
-    <h1>Se você leu isso, você é um programador sensacional! 💻</h1>
-    <img src="https://miro.medium.com/v2/resize:fit:1000/1*dxbvVHJkUh5HagZ7HI0nFw.gif">```
+    return (
+        <div>
+            <h1>Se você leu isso, você é um programador sensacional!! 💻</h1>
+            <img src="https://global.discourse-cdn.com/nubank/original/4X/7/a/a/7aa1c6d6b121fffe849a7c3313c7f22036df184c.gif" 
+            alt="Programador mandando joinha!"></img>
+        </div>
+    )
 }
 
 export default Home;
