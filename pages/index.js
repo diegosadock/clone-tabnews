53db37f2e639989865d3a82cfebaa326134aab3a
Diego Sadock <95287173+diegosadock@users.noreply.github.com>
2024-06-18 20:22:01 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Se você leu isso, você é um programador sensacional!</h1>
+    return ```
+    <h1>Se você leu isso, você é um programador sensacional! 💻</h1>
+    <img src="https://miro.medium.com/v2/resize:fit:1000/1*dxbvVHJkUh5HagZ7HI0nFw.gif">```
 }
 
 export default Home;
