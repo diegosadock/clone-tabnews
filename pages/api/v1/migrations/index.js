15fd8d8ed58160d879b95c443c671e59467e6e9f
Diego Sadock <95287173+diegosadock@users.noreply.github.com>
2024-07-20 17:06:12 +0000

    fix staging branch

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -17,6 +17,7 @@ export default async function migrations(request, response) {
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
     await dbClient.end();
+
     return response.status(200).json(pendingMigrations);
   }
 
